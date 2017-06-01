3a0bbeb0a730bae4e909475b92cb46f428dc4ee7
lucaslealvale <lucaslealvale01@gmail.com>
2017-06-01 09:56:26 -0300

    atualizcao na animacao world fase 9! lucas

--- animation_out_world.py
+++ animation_out_world.py
@@ -9,7 +9,7 @@ import main_code5 as game_5
 import main_code6 as game_6
 import main_code7 as game_7
 import main_code8 as game_8
-
+#import main_code9 as game_9
 
 def animation():
 
@@ -298,3 +298,36 @@ def animation8():
 
 		d=d+1
 		pygame.display.update()
+
+
+#def animation9():
+
+#	pygame.init()
+
+#	screen=pygame.display.set_mode((1200,720),0,32)
+
+#	pygame.display.set_caption('Physics, Morty')
+
+#	planeta=['sprite_terra00.png','sprite_terra01.png','sprite_terra02.png','sprite_terra03.png','sprite_terra04.png','sprite_terra05.png','sprite_terra06.png','sprite_terra07.png','sprite_terra08.png','sprite_terra09.png','sprite_terra10.png','sprite_terra11.png','sprite_terra12.png','sprite_terra13.png','sprite_terra14.png','sprite_terra15.png']
+
+#	d=0
+
+#	playing=True
+
+#	while playing:
+#		print(d)
+#		if d>15:
+#			game_8.oito()
+#		screen.fill([0,0,0])
+#		screen.blit(pygame.image.load(planeta[d]),[0,0])
+
+#		events=pygame.event.get()
+#		for event in events:
+#			if event.type==pygame.KEYDOWN:
+#				if event.key==K_ESCAPE:
+#					sys.exit()
+#			if event.type==pygame.QUIT:
+#				sys.exit()
+
+#		d=d+1
+#		pygame.display.update()
